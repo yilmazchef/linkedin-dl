092ed3e859a20b347f3a938cb50ea4ed4d51cdad
Yilmaz MUSTAFA <36330777+yilmazchef@users.noreply.github.com>
2022-01-06 10:28:31 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+# EDIT
+USERNAME = 'yilmaz@mail.be'
+PASSWORD = 'AA1a2z3e4rAA++'
+COURSES = [
+    'learning-python-2020'
+]
+
+COLLECTIONS = [
+
+]
+
+# EDIT IF YOU NEED TO
+BASE_DOWNLOAD_PATH = os.path.join(os.path.dirname(__file__), "downloads")
+USE_PROXY = False
+PROXY = "http://127.0.0.1:8888" if USE_PROXY else None
